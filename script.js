4f75bdde5c7fefeffa81b6e958821e8fe9332bbf
krifty <kaitlynmariejones@gmail.com>
2021-04-23 13:45:03 -0700

    grid color changes ashover over

--- script.js
+++ script.js
@@ -6,8 +6,20 @@ while (divNum < 256) {
   const container = document.querySelector('#container');
   const grid = document.createElement('div');
   grid.classList.add('grid');
+  grid.setAttribute("id", "grid" + divNum)
+  // grid.classList.add('grid' + divNum);
+  // console.log('grid' + divNum)
   // grid.textContent = divNum;
   container.appendChild(grid);
-  
-  
 }
+
+const grid = document.querySelectorAll(".grid");
+
+grid.forEach(hoover => {
+  hoover.addEventListener("mouseover", function(e) {
+    const gridNum = e.target.id;
+    document.getElementById(gridNum).style.backgroundColor = 'green' ; 
+      
+});
+})
+
